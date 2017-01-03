21ca8ad6711d14aa57d8e840d190ae9042e8fccb
Alex Cordonnier <alexjcord@gmail.com>
2017-01-02 23:47:44 -0500

    Greatly simplified token parsing by assuming tokens are valid and removing toString

--- firmware/TokenParser.h
+++ firmware/TokenParser.h
@@ -11,20 +11,17 @@ namespace Token {
         Times = 0x82,
         Div = 0x83,
     };
-
-    int size(Token t);
-    bool isValid(Token t);
-    const char* toString(Token t);
 };
 
 class TokenParser {
 public:
     TokenParser(uint8_t* data, int datalen);
     Token::Token nextToken();
-    String toString();
 
 private:
     uint8_t* _data;
     int _datalen;
     int _pos;
+
+    bool isA2ByteTok(uint8_t c);
 };
